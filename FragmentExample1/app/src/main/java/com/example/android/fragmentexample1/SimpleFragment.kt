aa6460567a5b1d799b90cb61bcdbaccbb546abb3
pamunoz <pfariasmunoz@gmail.com>
2019-04-12 08:04:37 -0400

    add the fragment to the layout

--- FragmentExample1/app/src/main/java/com/example/android/fragmentexample1/SimpleFragment.kt
+++ FragmentExample1/app/src/main/java/com/example/android/fragmentexample1/SimpleFragment.kt
@@ -2,10 +2,11 @@ package com.example.android.fragmentexample1
 
 
 import android.os.Bundle
-import android.app.Fragment
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_simple.*
 import kotlinx.android.synthetic.main.fragment_simple.view.*
 
