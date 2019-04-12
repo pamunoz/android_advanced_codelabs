9ad5c312aca8e0d5ab9f91a04461d59853cbb64f
pamunoz <pfariasmunoz@gmail.com>
2019-04-12 11:43:04 -0400

    Add a rating bar to the layout

--- FragmentExample1/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
+++ FragmentExample1/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
@@ -86,7 +86,7 @@ public final class R {
         public static final int spread_inside = 0x7f0700a6;
         public static final int start = 0x7f0700ab;
         public static final int top = 0x7f0700c0;
-        public static final int wrap = 0x7f0700c8;
+        public static final int wrap = 0x7f0700c9;
     }
     public static final class styleable {
         private styleable() {}
--- FragmentExample1/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/example/android/fragmentexample1/R.java
+++ FragmentExample1/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/com/example/android/fragmentexample1/R.java
@@ -3765,13 +3765,14 @@ public final class R {
     public static final int top=0x7f0700c0;
     public static final int topPanel=0x7f0700c1;
     public static final int tv_fragment_header=0x7f0700c2;
-    public static final int unchecked=0x7f0700c3;
-    public static final int uniform=0x7f0700c4;
-    public static final int up=0x7f0700c5;
-    public static final int useLogo=0x7f0700c6;
-    public static final int withText=0x7f0700c7;
-    public static final int wrap=0x7f0700c8;
-    public static final int wrap_content=0x7f0700c9;
+    public static final int tv_rating_label=0x7f0700c3;
+    public static final int unchecked=0x7f0700c4;
+    public static final int uniform=0x7f0700c5;
+    public static final int up=0x7f0700c6;
+    public static final int useLogo=0x7f0700c7;
+    public static final int withText=0x7f0700c8;
+    public static final int wrap=0x7f0700c9;
+    public static final int wrap_content=0x7f0700ca;
   }
   public static final class integer {
     public static final int abc_config_activityDefaultDur=0x7f080000;
