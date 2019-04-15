83ce91c9732984139316039eeebf379e6dfe5a95
pamunoz <pfariasmunoz@gmail.com>
2019-04-15 06:26:38 -0400

    Add project to test fragment communication

---
+++ FragmentCommunicate/app/src/main/java/com/example/android/fragmentcommunicate/MainActivity.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.fragmentcommunicate
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var isDisplayed = false
+
+    companion object {
+        const val STATE_FRAGMENT = "state_of_fragment"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        savedInstanceState?.let {
+            isDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT)
+            if (isDisplayed) btn_open.text = getString(R.string.close)
+        }
+
+        // Set the click listener for the button.
+        btn_open.setOnClickListener {
+            if (!isDisplayed) displayFragment() else closeFragment()
+        }
+    }
+
+    private fun displayFragment() {
+        // Add the SimpleFragment.
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.fragment_container, SimpleFragment.newInstance).addToBackStack(null)
+        }.commit()
+        //Update the Button text
+        btn_open.text = getString(R.string.close)
+        // Set boolean flag to indicate fragment is open.
+        isDisplayed = true
+    }
+
+    private fun closeFragment() {
+        // Check to see if the fragment is already showing.
+        val simpleFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as SimpleFragment
+        simpleFragment?.let { supportFragmentManager.beginTransaction().remove(it).commit() }
+        // Update the Button text.
+        btn_open.text = getString(R.string.open)
+        // Set boolean flag to indicate fragment is closed.
+        isDisplayed = false
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(STATE_FRAGMENT, isDisplayed)
+        super.onSaveInstanceState(outState)
+    }
+
+}
