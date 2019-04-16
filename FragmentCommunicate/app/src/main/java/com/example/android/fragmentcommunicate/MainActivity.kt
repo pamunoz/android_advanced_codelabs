9534774038bb8812ffae915774bdef9c17f4e029
pamunoz <pfariasmunoz@gmail.com>
2019-04-16 11:38:20 -0400

    Add a new constant

---
+++ FragmentCommunicate/app/src/main/java/com/example/android/fragmentcommunicate/MainActivity.kt
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.fragmentcommunicate
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var isFragmentDisplayed = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // If returning from a configuration change, get the
+        // fragment state and set the button text.
+        if (savedInstanceState != null) {
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT)
+            if (isFragmentDisplayed) {
+                // If the fragment is displayed, change button to "close".
+                open_button!!.setText(R.string.close)
+            }
+        }
+        // Set the click listener for the button.
+        open_button!!.setOnClickListener {
+            if (!isFragmentDisplayed) {
+                displayFragment()
+            } else {
+                closeFragment()
+            }
+        }
+    }
+
+    /**
+     * This method is called when the user clicks the button
+     * to open the fragment.
+     */
+    private fun displayFragment() {
+        // Instantiate the fragment.
+        val simpleFragment = SimpleFragment.newInstance()
+        // Get the FragmentManager and start a transaction.
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager
+                .beginTransaction()
+
+        // Add the SimpleFragment.
+        fragmentTransaction.add(R.id.fragment_container,
+                simpleFragment).addToBackStack(null).commit()
+
+        // Update the Button text.
+        open_button!!.setText(R.string.close)
+        // Set boolean flag to indicate fragment is open.
+        isFragmentDisplayed = true
+    }
+
+    /**
+     * This method is called when the user clicks the button to
+     * close the fragment.
+     */
+    private fun closeFragment() {
+        // Get the FragmentManager.
+        val fragmentManager = supportFragmentManager
+        // Check to see if the fragment is already showing.
+        val simpleFragment = fragmentManager
+                .findFragmentById(R.id.fragment_container) as SimpleFragment?
+        if (simpleFragment != null) {
+            // Create and commit the transaction to remove the fragment.
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.remove(simpleFragment).commit()
+        }
+        // Update the Button text.
+        open_button!!.setText(R.string.open)
+        // Set boolean flag to indicate fragment is closed.
+        isFragmentDisplayed = false
+    }
+
+    public override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        // Save the state of the fragment (true=open, false=closed).
+        savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    companion object {
+
+        // Saved instance state key.
+        internal const val STATE_FRAGMENT = "state_of_fragment"
+    }
+}
---
+++ FragmentCommunicate/app/src/main/java/com/example/android/fragmentcommunicate/SimpleFragment.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.fragmentcommunicate
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioGroup
+import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_simple.view.*
+
+/**
+ * A simple [Fragment] subclass that shows a question
+ * with radio buttons for providing feedback. If the user
+ * clicks "Yes" the text header changes to "Article: Like".
+ * If the user clicks "No" the text header changes to "Thanks".
+ */
+class SimpleFragment : Fragment() {
+
+    /**
+     * Creates the view for the fragment.
+     *
+     * @param inflater           LayoutInflater to inflate any views in the fragment
+     * @param container          ViewGroup of parent view to attach fragment
+     * @param savedInstanceState Bundle for previous state
+     * @return rootView
+     */
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment.
+        val rootView = inflater.inflate(R.layout.fragment_simple,
+                container, false)
+        //val radioGroup = rootView.findViewById<RadioGroup>(R.id.radio_group)
+
+        // Set the radioGroup onCheckedChanged listener.
+        rootView.radio_group.setOnCheckedChangeListener { _, checkedId ->
+            val radioButton = rootView.radio_group.findViewById<View>(checkedId)
+
+            when (rootView.radio_group.indexOfChild(radioButton)) {
+                YES // User chose "Yes".
+                -> rootView.fragment_header.text = getString(R.string.yes_message)
+                NO // User chose "No".
+                -> rootView.fragment_header.text = getString(R.string.no_message)
+                else // No choice made.
+                -> {
+                }
+            }// Do nothing.
+        }
+
+        // Return the View for the fragment's UI.
+        return rootView
+    }
+
+    companion object {
+
+        // The radio button choice has 3 states: 0 = yes, 1 = no,
+        // 2 = default (no choice). Using only 0 and 1.
+        private const val YES = 0
+        private const val NO = 1
+        private const val NONE = 2
+
+        fun newInstance(): SimpleFragment {
+            return SimpleFragment()
+        }
+    }
+}// Required empty public constructor
