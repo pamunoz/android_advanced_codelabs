f95524f0df6504b896071d39bcb4ce68018c54b6
pamunoz <pfariasmunoz@gmail.com>
2019-04-18 09:46:22 -0400

    Add the update section to the widget

--- AppWidgetSample/app/src/main/java/com/pfariasmunoz/appwidgetsample/NewAppWidget.kt
+++ AppWidgetSample/app/src/main/java/com/pfariasmunoz/appwidgetsample/NewAppWidget.kt
@@ -4,6 +4,8 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.widget.RemoteViews
+import java.text.DateFormat
+import java.util.*
 
 /**
  * Implementation of App Widget functionality.
@@ -19,17 +21,28 @@ class NewAppWidget : AppWidgetProvider() {
 
     companion object {
 
-        internal fun updateAppWidget(
-            context: Context, appWidgetManager: AppWidgetManager,
-            appWidgetId: Int
-        ) {
+        const val mSharedPrefFile = "com.pfariasmunoz.appwidgetsample"
+        const val COUNT_KEY = "count"
+
+        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+
+            val prefs = context.getSharedPreferences(mSharedPrefFile, 0)
+            var count = prefs.getInt(COUNT_KEY + appWidgetId, 0)
+            count++
+            val dateString = DateFormat.getTimeInstance(DateFormat.SHORT).format(Date())
 
             // Construct the RemoteViews object
             val views = RemoteViews(context.packageName, R.layout.new_app_widget)
+            views.setTextViewText(R.id.appwidget_update, context.resources.getString(R.string.widget_update, count, dateString))
             views.setTextViewText(R.id.appwidget_id, appWidgetId.toString())
 
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
+
+            prefs.edit().apply {
+                putInt(COUNT_KEY + appWidgetId, count)
+                apply()
+            }
         }
     }
 }
