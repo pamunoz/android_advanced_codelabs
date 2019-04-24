8fcc39c6013154a74230482a8de06780585445e0
pamunoz <pfariasmunoz@gmail.com>
2019-04-24 11:24:07 -0400

    Add click handler for changing the images

--- LargeImages/app/src/main/java/com/pfariasmunoz/largeimages/MainActivity.kt
+++ LargeImages/app/src/main/java/com/pfariasmunoz/largeimages/MainActivity.kt
@@ -2,11 +2,31 @@ package com.pfariasmunoz.largeimages
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    private var toggle = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeImage(view: View) {
+        toggle = when(toggle) {
+            0 -> {
+                view.setBackgroundResource(R.drawable.dinosaur_medium)
+                1
+            }
+            1 -> {
+                view.setBackgroundResource(R.drawable.ankylo)
+                2
+            }
+            else -> {
+                view.setBackgroundResource(R.drawable.dinosaur_large)
+                0
+            }
+        }
     }
 }
