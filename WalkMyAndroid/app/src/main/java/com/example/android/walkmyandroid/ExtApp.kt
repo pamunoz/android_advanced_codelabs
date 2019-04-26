1ac9059dab0017108a953fc66dd8295c54809cc6
pamunoz <pfariasmunoz@gmail.com>
2019-04-26 11:00:03 -0400

    Add extension functions

---
+++ WalkMyAndroid/app/src/main/java/com/example/android/walkmyandroid/ExtApp.kt
@@ -0,0 +1,30 @@
+package com.example.android.walkmyandroid
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
+
+fun Context.toast(message: Any, duration: Int = Toast.LENGTH_SHORT) {
+    when(message) {
+        is Int -> Toast.makeText(this, this.resources.getString(message), duration).show()
+        is String -> Toast.makeText(this, message, duration).show()
+    }
+}
+
+fun Activity.accessFineLocation(requestCode: Int) {
+    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestCode)
+    } else {
+        logd("getLocation: permissions granted")
+    }
+}
+
+fun Any.logd(message: String) {
+    val tag = this::class.java.simpleName
+    Log.d(tag, message)
+}
