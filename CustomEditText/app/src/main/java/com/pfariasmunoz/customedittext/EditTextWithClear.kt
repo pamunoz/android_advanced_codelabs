baabe10ebb4985c54ce300a0ff87fd2e0f067f71
pamunoz <pfariasmunoz@gmail.com>
2019-04-30 11:37:24 -0400

    Add a subclass of edit text

---
+++ CustomEditText/app/src/main/java/com/pfariasmunoz/customedittext/EditTextWithClear.kt
@@ -0,0 +1,8 @@
+package com.pfariasmunoz.customedittext
+
+import android.content.Context
+import androidx.appcompat.widget.AppCompatEditText
+
+class EditTextWithClear(context: Context?) : AppCompatEditText(context) {
+    
+}
