d998c5c6f369976c6e6b34b4dab9acb0e980eb86
pamunoz <pfariasmunoz@gmail.com>
2019-04-30 12:23:22 -0400

    Add secondary constructor and the drawable

--- CustomEditText/app/src/main/java/com/pfariasmunoz/customedittext/EditTextWithClear.kt
+++ CustomEditText/app/src/main/java/com/pfariasmunoz/customedittext/EditTextWithClear.kt
@@ -1,8 +1,23 @@
 package com.pfariasmunoz.customedittext
 
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.res.ResourcesCompat
 
-class EditTextWithClear(context: Context?) : AppCompatEditText(context) {
-    
+class EditTextWithClear : AppCompatEditText {
+
+    private val mClearButtonImage: Drawable?
+
+    init {
+        mClearButtonImage = ResourcesCompat.getDrawable(resources, R.drawable.ic_clear_opaque_24dp, null)
+    }
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+
+
+
 }
