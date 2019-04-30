300b2f7c16ab930d9a813e4d9cccf842b592d84c
pamunoz <pfariasmunoz@gmail.com>
2019-04-30 12:30:09 -0400

    Add method to hide or show the drawable

--- CustomEditText/app/src/main/java/com/pfariasmunoz/customedittext/EditTextWithClear.kt
+++ CustomEditText/app/src/main/java/com/pfariasmunoz/customedittext/EditTextWithClear.kt
@@ -17,7 +17,25 @@ class EditTextWithClear : AppCompatEditText {
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
+    /**
+     * Show the clear (X) button.
+     *  the setCompoundDrawablesRelativeWithIntrinsicBounds() method sets the drawable mClearButtonImage
+     *  to the end of the text. The method accommodates right-to-left (RTL) languages by using the
+     *  arguments as "start" and "end" positions rather than "left" and "right" positions.
+     */
+    private fun showClearButton(){
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,mClearButtonImage,null)
+    }
 
+    /**
+     * Hide the clear (X) button.
+     * Use null for positions that should not show a drawable. In hideClearButton(),
+     * the setCompoundDrawablesRelativeWithIntrinsicBounds() method replaces the
+     * drawable with null in the end position.
+     */
+    private fun hideClearButton(){
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null,null,null,null)
+    }
 
 
 }
