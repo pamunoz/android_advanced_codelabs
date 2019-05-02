892585af4be6d95fe9b18b15883dbf95387a4a92
pamunoz <pfariasmunoz@gmail.com>
2019-05-02 14:15:52 -0400

    Add the variables that will be use in the app

--- SimpleCanvas/app/src/main/java/com/pfariasmunoz/simplecanvas/MainActivity.kt
+++ SimpleCanvas/app/src/main/java/com/pfariasmunoz/simplecanvas/MainActivity.kt
@@ -1,9 +1,33 @@
 package com.pfariasmunoz.simplecanvas
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private var mCanvas: Canvas? = null
+    private val mPaint: Paint = Paint()
+    private val mPaintText: Paint = Paint(Paint.UNDERLINE_TEXT_FLAG)
+    private var mBitmap: Bitmap? = null
+    private val mRect: Rect = Rect()
+    private val mBounds: Rect = Rect()
+    private var mOffset = OFFSET
+
+    private var mColorBackground: Int = 0
+    private var mColorRectangle: Int = 0
+    private var mColorAccent: Int = 0
+
+    companion object {
+        /** This offset is the distance of a rectangle you draw from the edge of the canvas. */
+        const val OFFSET = 120
+        /** You will need this constant later, for generating random colors. */
+        const val MULTIPLIER = 100
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
