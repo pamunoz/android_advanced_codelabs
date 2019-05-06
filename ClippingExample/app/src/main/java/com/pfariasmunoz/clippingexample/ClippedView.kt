a2fca984d82d4711fca67c0306f81d8de61391c6
pamunoz <pfariasmunoz@gmail.com>
2019-05-06 14:45:23 -0400

    Create a customview with it path and paint

---
+++ ClippingExample/app/src/main/java/com/pfariasmunoz/clippingexample/ClippedView.kt
@@ -0,0 +1,23 @@
+package com.pfariasmunoz.clippingexample
+
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.View
+
+class ClippedView : View {
+
+    private var mPaint: Paint? = null
+    private var mPath: Path? = null
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    )
+}
