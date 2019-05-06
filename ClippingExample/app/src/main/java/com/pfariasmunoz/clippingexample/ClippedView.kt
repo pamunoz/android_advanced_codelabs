890b812f803f8440b1c1c4eae9e356f3f016a2c0
pamunoz <pfariasmunoz@gmail.com>
2019-05-06 16:44:58 -0400

    Change the constructor

--- ClippingExample/app/src/main/java/com/pfariasmunoz/clippingexample/ClippedView.kt
+++ ClippingExample/app/src/main/java/com/pfariasmunoz/clippingexample/ClippedView.kt
@@ -5,7 +5,6 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.Rect
 import android.graphics.RectF
-import android.os.Build
 import android.util.AttributeSet
 import android.view.View
 
