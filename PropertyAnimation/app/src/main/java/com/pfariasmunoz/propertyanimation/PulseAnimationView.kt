5755af254a286059277176c5ce86ceb53ffa9282
pamunoz <pfariasmunoz@gmail.com>
2019-05-08 17:58:28 -0400

    Add the class for the animations

---
+++ PropertyAnimation/app/src/main/java/com/pfariasmunoz/propertyanimation/PulseAnimationView.kt
@@ -0,0 +1,9 @@
+package com.pfariasmunoz.propertyanimation
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PulseAnimationView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+}
