c834d203ef8f0b0a607aaadc3b14fa73fa563425
pamunoz <pfariasmunoz@gmail.com>
2019-05-08 12:37:09 -0400

    Initialize gameview and set it fullscreen

--- SurfaceViewExample/app/src/main/java/com/pfariasmunoz/surfaceviewexample/MainActivity.kt
+++ SurfaceViewExample/app/src/main/java/com/pfariasmunoz/surfaceviewexample/MainActivity.kt
@@ -4,15 +4,24 @@ import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
+    private var mGameView: GamveView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        
         // Lock the screen orientation into landscape. Games often lock the screen orientation.
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        // Create an instance of GameView.
+        mGameView = GamveView(this).apply {
+            // Set mGameView to completely fill the screen.
+            systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        }
+        // Set mGameView as the content view for MainActivity.
+        setContentView(mGameView)
 
     }
 }
