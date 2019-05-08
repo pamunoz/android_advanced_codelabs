2227e5defaca80b696280be7dafe014bd3c7385d
pamunoz <pfariasmunoz@gmail.com>
2019-05-08 12:32:28 -0400

    Lock the orientation to landscape

--- SurfaceViewExample/app/src/main/java/com/pfariasmunoz/surfaceviewexample/MainActivity.kt
+++ SurfaceViewExample/app/src/main/java/com/pfariasmunoz/surfaceviewexample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pfariasmunoz.surfaceviewexample
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,9 +11,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fl = FlashlightCone(200, 200)
-        Log.d("FLASH", "BEFORE X:${fl.x} AND Y: ${fl.y}")
-        fl.update(3, 4)
-        Log.d("FLASH", "AFTER X:${fl.x} AND Y: ${fl.y}")
+        // Lock the screen orientation into landscape. Games often lock the screen orientation.
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
     }
 }
