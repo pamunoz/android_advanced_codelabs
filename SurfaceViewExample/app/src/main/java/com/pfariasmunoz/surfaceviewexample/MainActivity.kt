4cccec2c39ded04892929441cf0d9a1c799be4e5
pamunoz <pfariasmunoz@gmail.com>
2019-05-08 12:26:30 -0400

    Add a class that represent the cone of the flashlight

--- SurfaceViewExample/app/src/main/java/com/pfariasmunoz/surfaceviewexample/MainActivity.kt
+++ SurfaceViewExample/app/src/main/java/com/pfariasmunoz/surfaceviewexample/MainActivity.kt
@@ -2,11 +2,17 @@ package com.pfariasmunoz.surfaceviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fl = FlashlightCone(200, 200)
+        Log.d("FLASH", "BEFORE X:${fl.x} AND Y: ${fl.y}")
+        fl.update(3, 4)
+        Log.d("FLASH", "AFTER X:${fl.x} AND Y: ${fl.y}")
     }
 }
