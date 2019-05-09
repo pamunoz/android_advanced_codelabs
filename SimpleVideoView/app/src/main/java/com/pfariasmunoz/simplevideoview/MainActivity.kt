a5c76da02a3f30d60230fed9e90c280b9738d408
pamunoz <pfariasmunoz@gmail.com>
2019-05-09 12:21:35 -0400

    Add tha constant for the name of the video

--- SimpleVideoView/app/src/main/java/com/pfariasmunoz/simplevideoview/MainActivity.kt
+++ SimpleVideoView/app/src/main/java/com/pfariasmunoz/simplevideoview/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val VIDEO_SAMPLE = "tacoma_narrows"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
