378d32e7492d0035445f21f6f870c1efab78c12c
pamunoz <pfariasmunoz@gmail.com>
2019-05-09 10:42:07 -0400

    Change the color when adjusting the radius

--- PropertyAnimation/app/src/main/java/com/pfariasmunoz/propertyanimation/PulseAnimationView.kt
+++ PropertyAnimation/app/src/main/java/com/pfariasmunoz/propertyanimation/PulseAnimationView.kt
@@ -1,6 +1,7 @@
 package com.pfariasmunoz.propertyanimation
 
 import android.content.Context
+import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
@@ -10,9 +11,11 @@ class PulseAnimationView(context: Context?, attrs: AttributeSet? = null) : View(
     var radius: Float = 0.0f
         set(value) {
             field = value
+            // adjust the color
+            mPaint.color = Color.GREEN + radius.toInt() / COLOR_ADJUSTER
             invalidate()
         }
-    val mPaint: Paint = Paint()
+    private val mPaint: Paint = Paint()
 
     companion object {
         const val COLOR_ADJUSTER = 5
