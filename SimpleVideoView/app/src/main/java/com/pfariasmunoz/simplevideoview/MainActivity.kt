080cac88622dc680269922b3a41706b5e4cafafb
pamunoz <pfariasmunoz@gmail.com>
2019-05-09 12:25:33 -0400

    Add method to get the path as Uri of the file

--- SimpleVideoView/app/src/main/java/com/pfariasmunoz/simplevideoview/MainActivity.kt
+++ SimpleVideoView/app/src/main/java/com/pfariasmunoz/simplevideoview/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pfariasmunoz.simplevideoview
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -13,4 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /**
+     * This method converts the name of the sample media file (a string) into a full URI object
+     * representing the path to the sample in your app's resources. Note that although the
+     * actual filename in the app's resource directory is tacoma_narrows.mp4,
+     * the string name and resulting resource name do not include the extension.
+     */
+    private fun getMedia(mediaName: String): Uri = Uri.parse("android:resource://$packageName/raw/$mediaName")
 }
