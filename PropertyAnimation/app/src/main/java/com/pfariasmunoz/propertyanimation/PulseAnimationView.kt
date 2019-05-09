8229f19ba1fc939718c0f40025c47fe5c35ad0d7
pamunoz <pfariasmunoz@gmail.com>
2019-05-09 10:30:56 -0400

    Add the customview to the layout

--- PropertyAnimation/app/src/main/java/com/pfariasmunoz/propertyanimation/PulseAnimationView.kt
+++ PropertyAnimation/app/src/main/java/com/pfariasmunoz/propertyanimation/PulseAnimationView.kt
@@ -4,6 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
-class PulseAnimationView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+class PulseAnimationView(context: Context?, attrs: AttributeSet? = null) : View(context, attrs) {
 
 }
