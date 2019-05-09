036348eeb90e4deb886c65c568913ffc1ae182d0
pamunoz <pfariasmunoz@gmail.com>
2019-05-09 10:46:29 -0400

    Respond to touch events

--- PropertyAnimation/app/src/main/java/com/pfariasmunoz/propertyanimation/PulseAnimationView.kt
+++ PropertyAnimation/app/src/main/java/com/pfariasmunoz/propertyanimation/PulseAnimationView.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class PulseAnimationView(context: Context?, attrs: AttributeSet? = null) : View(context, attrs) {
@@ -16,9 +17,19 @@ class PulseAnimationView(context: Context?, attrs: AttributeSet? = null) : View(
             invalidate()
         }
     private val mPaint: Paint = Paint()
+    private var mXpos: Float = 0f
+    private var mYpos: Float = 0f
 
     companion object {
         const val COLOR_ADJUSTER = 5
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.actionMasked == MotionEvent.ACTION_DOWN) {
+            mXpos = event.x
+            mYpos = event.y
+        }
+        return super.onTouchEvent(event)
     }
 
 }
